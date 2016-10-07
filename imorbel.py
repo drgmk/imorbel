bef531f0351d158ac9eba7a5712546069c21f8ea
drgmk <gkennedy@ast.cam.ac.uk>
2016-10-07 18:28:36 +0100

    command line version

--- imorbel.py
+++ imorbel.py
@@ -1,141 +1,218 @@
+#!/usr/bin/env python
+
 import numpy as np
 from multiprocessing import Pool
 import corner
 from funcs import *
 
-def elem(t,N,Nerr,E,Eerr,M,Merr,d,derr):
+# run from the command line
+if __name__ == "__main__":
 
-    # samples of x_0, y_0, V and direction and best fit
-    samples = velfit(t,N,Nerr,E,Eerr)
+    # inputs
+    parser = argparse.ArgumentParser(description='imorbel - orbital constraints for linear motion')
+
+    parser1 = parser.add_mutually_exclusive_group(required=True)
+    parser1.add_argument('--sep',type=float,nargs='+',help='Radial separations')
+    parser1.add_argument('--N',type=float,nargs='+',help='North separations')
+
+    parser.add_argument('--e_sep',type=float,nargs='+',help='Radial separation uncertainties')
+    parser.add_argument('--e_N',type=float,nargs='+',help='North separation unceratinties')
+
+    parser2 = parser.add_mutually_exclusive_group(required=True)
+    parser2.add_argument('--pa',type=float,nargs='+',help='Position angles (E of N)')
+    parser2.add_argument('--E',type=float,nargs='+',help='East separations')
+
+    parser.add_argument('--e_pa',type=float,nargs='+',help='Position angle uncertainties')
+    parser.add_argument('--e_E',type=float,nargs='+',help='East separation unceratinties')
+
+    parser.add_argument('--date',type=str,nargs='+',help='Dates (YYYY-MM-DD)',required=True)
+
+    parser.add_argument('--mass',type=float,help='Stellar mass (Msun)',required=True)
+    parser.add_argument('--e_mass',type=float,help='Stellar mass uncertainty (Msun)',required=True)
+
+    parser.add_argument('--distance',type=float,help='Distance (pc)',required=True)
+    parser.add_argument('--e_distance',type=float,help='Distance uncertainty (pc)',required=True)
+
+    parser.add_argument('--nzvz',type=int,help='Number of z/vz grid points',default=100)
+    parser.add_argument('--nelem',type=int,help='Number of element samples (approx)',default=10000)
+
+    parser.add_argument('--Qmax',type=float,help='Max apocenter to plot',default=1000.0)
+
+    parser.add_argument('--zvzfile',type=str,help='zvz file name',default='zvz.png')
+    parser.add_argument('--elemfile',type=str,help='elem file name',default='elem_noerr.png')
+
+    parser.add_argument('--interactive','-i',action='store_true',help='Interactive plot')
+
+    args = parser.parse_args()
+
+    # convert lists of floats to numpy ndarrays
+    args.sep = np.array(args.sep,dtype=float)
+    args.e_sep = np.array(args.e_sep,dtype=float)
+    args.pa = np.array(args.pa,dtype=float)
+    args.e_pa = np.array(args.e_pa,dtype=float)
+    args.N = np.array(args.N,dtype=float)
+    args.e_N = np.array(args.e_N,dtype=float)
+    args.E = np.array(args.E,dtype=float)
+    args.e_E = np.array(args.e_E,dtype=float)
+    
+    # convert so we can do things in cartesians
+    if len(args.sep) > 0:
+        N,e_N,E,e_E = seppa2cart(args.sep,args.e_sep,args.pa*np.pi/180.,args.e_pa*np.pi/180.)
+    else:
+        N,e_N,E,e_E = args.N,args.e_N,args.E,args.e_E
+
+    # convert the dates
+    t = Time(args.date).decimalyear
+        
+    # get samples of x_0, y_0, V and direction and best fit
+    nwalk = 32
+    samples = velfit(t,N,e_N,E,e_E,
+                     nwalkers=nwalk,nruns=1000,
+                     plotsky=True,plottri=True)
     best = np.median(samples,axis=0)
 
-    # best fit parameters
-    R = np.sqrt(best[0]**2 + best[1]**2) * d
+    # extract the best fit parameters
+    R = np.sqrt(best[0]**2 + best[1]**2) * args.distance
     pa0 = np.arctan2(best[1],best[0])-np.pi/2. # from E of N, as in samples
-    V = best[2] * d
-    B = V**2 * R / (8*np.pi**2 * M)
+    V = best[2] * args.distance
+    B = V**2 * R / (8*np.pi**2 * args.mass)
+    phi = best[3] - pa0
+    if phi > np.pi:
+        phi -= 2*np.pi
+    zsgn = 1
+    if phi < 0:
+        zsgn = -1
+        phi = -1. * phi
 
-    # distributions of M, d, B
-    Mdist = np.random.normal(loc=M,scale=Merr,size=len(samples))
-    ddist = np.random.normal(loc=d,scale=derr,size=len(samples))
-    Rdist = np.sqrt(samples[:,0]**2 + samples[:,1]**2) * ddist
-    Vdist = samples[:,2] * ddist
-    Bdist = Vdist**2 * Rdist / (8*np.pi**2 * Mdist)
-    pa0dist = np.arctan2(samples[:,1],samples[:,0])-np.pi/2. # from E of N
+    print('')
+    print('R =', R, 'au')
+    print('V =', V, 'au / yr')
+    print('B =', B)
+    print('phi =', phi/np.pi*180, 'deg')
+    print('PA0 =', pa0/np.pi*180, 'deg')
+    print("Zsign =",zsgn)
+    print('')
 
-    zsgn = np.ones(len(samples))
-    phidist = samples[:,3] - pa0dist
-    phidist += 2.*np.pi
-    # ensure phi is defined correctly
-    while np.max(phidist) >= np.pi:
-        phidist[(phidist >= np.pi)] -= 2.*np.pi
-    if np.any(phidist < 0.):
-        neg = (phidist < 0.)
-        zsgn[neg] = -1.
-        phidist[neg] = 2.*np.pi - phidist[neg]
-        if np.any(phidist > 2.*np.pi):
-            gt = (phidist > 2.*np.pi)
-            phidist[gt] -= 2.*np.pi
-    phidist *= 180./np.pi
+    titlestr = str(args)+'\n\n'
 
-    plsamples = np.array(samples)
-    plsamples[:,3] *= 180./np.pi
-    extra = np.column_stack((plsamples,Mdist,ddist,Bdist,phidist))
-    labels = ('$x_0$/arcsec','$y_0$/arcsec','$V$/arcsec/yr','$PA_V/^\circ$',
-              '$M_\star/M_\odot$','$d/pc$','$B$','$\phi/^\circ$')
-    fig = corner.corner(extra,labels=labels,quantiles=[0.16, 0.5, 0.84],show_titles=True)
-    fig.savefig('inparams_tri.png')
-    plt.close(fig)
+    # use these contour levels
+    contour_levels = default_contour_levels()
 
-    N_z = 500
-    N_vz = 500
+    # zvz plot for best fit
+    z_vz_data = get_z_vz_data(R,V,B,args.nzvz,args.nzvz)
 
-    # Define tested region of z, vz space
-    z_vz_data = get_z_vz_data(R,V,B,N_z,N_vz)
+    # Cycle through z, vz values, and derive orbital elements at each set of values
+    element_matrices = get_element_grids(z_vz_data,R,V,B,phi)
 
-    # parallel
-    nrun = 1000000
-    j = np.random.randint(len(samples),size=nrun)
-    dist = np.random.normal(loc=d,scale=derr,size=nrun)
-    mass = np.random.normal(loc=M,scale=Merr,size=nrun)
-    R = np.sqrt(samples[j,0]**2 + samples[j,1]**2) * dist
-    pa0 = np.arctan2(samples[j,1],samples[j,0])-np.pi/2. # from E of N
-    V = samples[j,2] * dist
-    B = V**2 * R / (8*np.pi**2 * mass)
-    zsgn = np.ones(nrun)
-    phi = samples[j,3] - pa0
-    phi += 2.*np.pi
-    # ensure phi is defined correctly
-    while np.max(phi) >= np.pi:
-        phi[(phi >= np.pi)] -= 2.*np.pi
-    if np.any(phi < 0.):
-        neg = (phi < 0.)
-        zsgn[neg] = -1.
-        phi[neg] = 2.*np.pi - phi[neg]
-        if np.any(phi > 2.*np.pi):
-            gt = (phi > 2.*np.pi)
-            phi[gt] -= 2.*np.pi
-    # swap sign of z when phi would have been negative
-    z = z_vz_data['z_list'][np.random.randint(N_z,size=nrun)] * zsgn
-    vz = z_vz_data['vz_list'][np.random.randint(N_vz,size=nrun)] * zsgn
-    pars = []
-    pars = [np.append(pars,[z[i],vz[i],R[i],V[i],B[i],phi[i]]) for i in range(nrun)]
-    pool = Pool(processes=16)
-    el = pool.map(calc_elements_array,pars)
-    el = np.array(el)
+    # do interactive plot for best fit and exit when window is closed
+    if args.interactive:
+        plt.close('all')
+        interactive_contour_plot(z_vz_data, element_matrices, contour_levels,R,V,B,phi,pa0,zsgn)
+        exit
 
-    ok = (el[:,1] > 0 ) & (el[:,3] < 1) & (el[:,2] < 50)
-    fig = corner.corner(el[ok],color='k',top_ticks=True,bins=50,
-                        labels=('$a/au$','$q/au$','$Q/au$',
-                                '$e$','$I/^\circ$','$\Omega/^\circ$',
-                                '$\omega/^\circ$',r'$\varpi/^\circ$','$f/^\circ$'),
-                        range=[1.,(0.,np.max(el[:,1])),1.,(0,1),(0,90),(0,360),(0,360),(0,360),(0,360)])
-    fig.savefig('elem_corner.png')
-    plt.close(fig)
+    # or make plots
+    else:
 
-# data
-info = getsys('HD 206893')
-t,N,Nerr,E,Eerr,M,Merr,d,derr = info
-#print(info)
+        # z/vz contour plots for best fit
+        make_contour_plots(z_vz_data, element_matrices, contour_levels, args.zvzfile, titlestr)
 
-elem(t,N,Nerr,E,Eerr,M,Merr,d,derr)
+        # get distributions of M, d, B
+        Mdist = np.random.normal(loc=args.mass,scale=args.e_mass,size=len(samples))
+        ddist = np.random.normal(loc=args.distance,scale=args.e_distance,size=len(samples))
+        Rdist = np.sqrt(samples[:,0]**2 + samples[:,1]**2) * ddist
+        Vdist = samples[:,2] * ddist
+        Bdist = Vdist**2 * Rdist / (8*np.pi**2 * Mdist)
+        pa0dist = np.arctan2(samples[:,1],samples[:,0])-np.pi/2. # from E of N
 
+        # ensure phi is in the correct range of -pi..pi
+        phidist = samples[:,3] - pa0dist
+        while np.min(phidist) <= -np.pi:
+            phidist[(phidist <= -np.pi)] += 2.*np.pi
+        while np.max(phidist) >= np.pi:
+            phidist[(phidist >= np.pi)] -= 2.*np.pi
 
-# unused single processor version
+        # sort out angles, zsgn is -ve when orbit would need to go clockwise (so it is
+        # instead viewed from the other size of the sky plane) and is then viewed from
+        # -ve z (so phi -> 360-phi)
+        zsgn = np.ones(len(samples))
+        if np.any(phidist < 0.):
+            neg = (phidist < 0.)
+            zsgn[neg] = -1.
+            phidist[neg] = -1 * phidist[neg]
+        phidist *= 180./np.pi
+        pa0dist *= 180./np.pi
 
-##     if 0:
-##         rvbphi = []
-##         el = []
-##         for i in range(100000):
-            
-##             # select random R, V, B, phi from samples
-##             j = np.random.randint(len(samples))
-##             dist = np.random.normal(loc=d,scale=derr)
-##             mass = np.random.normal(loc=M,scale=Merr)
-##             R = np.sqrt(samples[j][0]**2 + samples[j][1]**2) * dist
-##             pa0 = np.arctan2(samples[j][1],samples[j][0]) # from x, not E of N
-##             V = samples[j][2] * dist
-##             B = V**2 * R / (8*np.pi**2 * mass)
-##             zsgn = 1.
-##             phi = samples[j][3] - pa0
-##         #    print(samples[j][3],pa0,phi)
-##             # ensure phi is defined correctly
-##             while phi >= np.pi: phi -= 2.*np.pi
-##         #    print(phi)
-##             if -np.pi < phi < 0.:
-##                 zsgn = -1.
-##                 phi = 2.*np.pi - phi
-##                 if phi > 2.*np.pi: phi -= 2.*np.pi
-##         #    print(j,R,V,B,phi,pa0,zsgn)
-##             # swap sign of z when phi would have been negative
-##             z = z_vz_data['z_list'][np.random.randint(N_z)] * zsgn
-##             vz = z_vz_data['vz_list'][np.random.randint(N_vz)] * zsgn
-            
-##             # compute
-##             a = calc_elements_array([z,vz,R,V,B,phi])
-##             if len(el) == 0:
-##                 el = np.append(el,a)
-##                 rvbphi = np.append(rvbphi,[R,V,B,phi,zsgn,z,vz])
-##             else:
-##                 el = np.vstack((el,a))
-##                 rvbphi = np.vstack((rvbphi,[R,V,B,phi,zsgn,z,vz]))
+        # plot the samples from the fit to the astrometry
+        extra = np.column_stack((Rdist,Vdist,pa0dist,Bdist,phidist,Mdist,ddist))
+        labels = ('$R/au$','$V/au/yr$','$PA_V/^\circ$',
+                  '$B$','$\phi/^\circ$',
+                  '$M_\star/M_\odot$','$d/pc$',)
+        fig = corner.corner(extra,labels=labels,quantiles=[0.16, 0.5, 0.84],show_titles=True)
+        fig.savefig('inparams_tri.png')
+        plt.close(fig)
+
+        # Define tested region of z, vz space, maximising by choosing the largest B and
+        # smallest R and V (c.f. Pearce+15 eq 4) we will throw away more samples, but
+        # won't miss them this way
+        z_vz_data = get_z_vz_data(np.min(R),np.min(V),np.max(B),args.nzvz,args.nzvz)
+
+        # compute larger random sample, repeatedly sample from samples, but additional
+        # randomisation comes from random sampling of distance and mass
+        j = np.random.randint(len(samples),size=args.nelem)
+        ddist = np.random.normal(loc=args.distance,scale=args.e_distance,size=args.nelem)
+        Mdist = np.random.normal(loc=args.mass,scale=args.e_mass,size=args.nelem)
+        Rdist = np.sqrt(samples[j,0]**2 + samples[j,1]**2) * ddist
+        pa0dist = np.arctan2(samples[j,1],samples[j,0]) - np.pi/2. # from E of N
+        Vdist = samples[j,2] * ddist
+        Bdist = Vdist**2 * Rdist / (8*np.pi**2 * Mdist)
+        phidist = samples[j,3] - pa0dist
+
+        # same as above
+        while np.min(phidist) <= -np.pi:
+            phidist[(phidist <= -np.pi)] += 2.*np.pi
+        while np.max(phidist) >= np.pi:
+            phidist[(phidist >= np.pi)] -= 2.*np.pi
+        zsgn = np.ones(args.nelem)
+        if np.any(phidist < 0.):
+            neg = (phidist < 0.)
+            zsgn[neg] = -1.
+            phidist[neg] = -1 * phidist[neg]
+
+        # z and vz arrays
+        z = z_vz_data['z_list'][np.random.randint(args.nzvz,size=args.nelem)]
+        vz = z_vz_data['vz_list'][np.random.randint(args.nzvz,size=args.nelem)]
+
+        # grab a series of random elements, these are correlated so need to select the
+        # same i from each
+        pars = []
+        pars = [np.append(pars,[z[i],vz[i],Rdist[i],Vdist[i],Bdist[i],phidist[i]]) for i in range(args.nelem)]
+                             
+        pool = Pool(processes=8)
+        el = pool.map(calc_elements_array,pars)
+        el = np.array(el)
+
+        # select a subset of these, pericenter must be +ve and e<1
+        ok = (el[:,1] > 0) & (el[:,3] < 1)
+        # and apocenter < Qmax
+        ok1 = (el[:,2] < args.Qmax)
+
+        ok = np.all([ok,ok1],axis=0)
+
+        # make corner plot, size pasted from corner code
+        K = len(el[0])
+        factor = 2.0           # size of one side of one panel
+        lbdim = 0.5 * factor   # size of left/bottom margin
+        trdim = 0.2 * factor   # size of top/right margin
+        whspace = 0.05         # w/hspace size
+        plotdim = factor * K + factor * (K - 1.) * whspace
+        dim = lbdim + plotdim + trdim
+
+        fig,axes = plt.subplots(len(el[0]),len(el[0]),figsize=(dim,dim))
+        fig = corner.corner(el[ok],color='k',top_ticks=True,bins=50,fig=fig,
+                            labels=('$a/au$','$q/au$','$Q/au$',
+                                    '$e$','$I/^\circ$','$\Omega/^\circ$',
+                                    '$\omega/^\circ$',r'$\varpi/^\circ$','$f/^\circ$'),
+                            range=[1.,(0.,np.max(el[:,1])),1.,(0,1),(0,90),(0,360),(0,360),(0,360),(0,360)])
+        axes[1,4].set_title(titlestr)
+        fig.savefig('elem.png')
+        plt.close(fig)
